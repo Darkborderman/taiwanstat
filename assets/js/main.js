5b58745d52291f8132fd7dc7132512a24d12cb43
darkborderman <reastw1234@gmail.com>
2018-09-09 23:07:14 +0800

    ==update csv file to 1d format== update csv file from 2d to 1d format can display square on graph
    TODO:
    Draw line between each line

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,91 +11,89 @@ let height=Setting.graph.height-Setting.graph.margin.top-Setting.graph.margin.bo
 let graph=svg.append("g")
     .attr("transform", "translate(" + Setting.graph.margin.left + "," + Setting.graph.margin.top + ")");
 
-//load json
-d3.json("assets/json/initial_staff_wage.json", function (error, data) {
-
+//load csv
+d3.csv(`assets/csv/青年勞工初次尋職時選擇工作的考慮因素(fin)/total-2.csv`, function (error, data) {
 
     if (error) throw error;
+    else console.log(data);
 
-    //due to v4  version, ordinal range need to be insert each delta
-    let xDomain=[]
-    for(let i=0;i<=data.length-1;i++)
+    //Y-axis
+
+    let y=d3.scaleLinear().domain([0,60]).range([height,0]);
+
+    graph.append("g")
+    .attr("class", "y axis")
+    .call(d3.axisLeft(y));
+
+    graph.append("g")
+    .attr("class", "y unit")
+    .append("text")
+    .attr("y", -10)
+    .attr("x", -50)
+    .text("比率(%)");
+
+    //X-axis
+
+    let xDomain=[];
+    for(item in data)
     {
-        if(!data[i]["大職業別"].split("-")[1]){
-            xDomain.push(data[i]["大職業別"].split("-")[0]);
+        if(item!=`columns`)
+        {
+            if(xDomain.indexOf(data[item][`year`])==-1){
+                xDomain.push(data[item][`year`]);
+            }
         }
     }
     let xAxis=[];
     let xUnit=width/xDomain.length;
         
     for(let i=0;i<=xDomain.length-1;i++){
-        xAxis[i]=xUnit*i;
+        xAxis[i]=xUnit*i+50;
     }
-    let x = d3.scaleOrdinal().range(xAxis);
-    let y = d3.scaleLinear().range([height, 0]);
-
-    x.domain(xDomain);
+    let x = d3.scaleOrdinal().range(xAxis).domain(xDomain);
 
     graph.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x))
         .selectAll("text")
-            .attr("x",30)
-            .attr("y",30)
-            .attr("transform","rotate(40)");
+            .attr("x",0)
+            .attr("y",20)
 
-    y.domain([21000,35000]);
+    //Data insert
 
-    graph.append("g")
-        .attr("class", "y axis")
-        .call(d3.axisLeft(y));
-    
-    graph.append("g")
-        .attr("class", "y unit")
-        .append("text")
-        .attr("y", -10)
-        .attr("x", -50)
-        .text("薪資(新台幣)");
-
-    //add data
-    graph.selectAll("bar")
+        graph.selectAll("bar")
         .data(data)
-        .enter().append("rect")
+        .enter()
+        .append("rect")
         .style("opacity",0.7)
-        .attr("class",function(d){
-            let department=d["大職業別"].split("-")[0];
-            let staffKind=d["大職業別"].split("-")[1];
-            if(staffKind!=undefined)
-            {
-                return `${department} ${staffKind}`;
-            }
-            else return department;
-        })
-        .attr("width", 10)
-        .attr("height", 10)
-        .attr("x",(d)=>{ return x( d["大職業別"].split("-")[0])})
-        .attr("y", function (d) { return y(d["經常性薪資"]) })
-        .on("mouseover",function(d){
-            //console.log(d);
-            d3.select(this)
-                .style("opacity",1.0);
-            graph.append("text")
-                .attr("id","node"+d["大職業別"])
-                .attr("x",x( d["大職業別"].split("-")[0])+10)
-                .attr("y",y(d["經常性薪資"]-10))
-                .text(d["經常性薪資"]);
-        })
-        .on("mouseout",function(d){
-            d3.select(this)
-                .style("opacity",0.7)
-            d3.select("#node"+d["大職業別"])
-            .remove();
-        })
-        .on("click",function(d){
-            console.log(d);
-            window.scrollTo(0,document.body.scrollHeight);
-            document.getElementById("apartment").innerHTML=d["大職業別"];
-            document.getElementById("staffKind").innerHTML=`受僱員工平均薪資: ${d["經常性薪資"]} 新台幣`
+        .attr("width",7)
+        .attr("height",7)
+        .attr("x",(d)=>{return x(d[`year`]);})
+        .attr("y",(d)=>{return y(d[`value`]);})
+        .attr("class",(d)=>{
+            return `${d[`year`]} ${d[`type`]}`;
         });
+
+
+    //draw line
+    /*
+    // define the line
+    let valueline = d3.line()
+    .x(function(d) { 
+        console.log(d[`year`])
+        return x(d[`year`]); })
+    .y(function(d) { return y(d[`value`]); });
+
+    //console.log(valueline);
+    graph.selectAll("rect")
+    .data(data)
+    .append('path')
+    .attr('d',function(d){
+        console.log(x(d[`year`]))
+        console.log(y(d[`value`]))
+        return valueline(d)}
+    )
+    .attr('fill',"black")
+    */
 });
